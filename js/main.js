e5c29025ad1c5be18869d5dee78c441f36ebc23f
dramirez <diego.ramirezh@sansano.usm.cl>
2022-06-07 13:38:47 -0400

    Cambio de diseño + 2 tablas
    Se mejora el diseño además de separar listos y faltantes en tablas respectivas
    - revisar porsibles bugs
    - cuando se selecciona o saca todas las animaciones se restablecen por usar la funcion (ordenarTablas() en main.js)

--- js/main.js
+++ js/main.js
@@ -4,19 +4,19 @@ $(async () => {
     let fecha = moment();
 
     const listarUsuarios = async() => {
-        $(".fecha").html(`Fecha: ${fecha.format("DD/MM/YYYY")}`)
+        $(".fecha").append(`<h3>Información daily  ${fecha.format("DD/MM/YYYY")}</h3>`)
         let usuarios = await fetch(`${server}/usuarios.json`);
         usuarios = await usuarios.json();
         usuarios.sort((a,b) => a > b ? 1 : -1);
-        usuarios.forEach(usuario => {
+        usuarios.forEach((usuario, index) => {
             if(usuario){
-                $(".usuarios").append(`
-                    <div class='mb-4 mb-md-2 ps-3 ps-md-0'>
-                        <input class="form-check-input input-registro" data-name='${usuario}' type="checkbox" value="">
-                            <label class="form-check-label" for="flexCheckChecked">
-                            ${usuario}
-                        </label>
-                    </div>
+                $(".faltantes").append(`
+                <div id="${usuario}" style="display:contents;">
+                <input id="${index}" class="form-check-input input-registro" data-name='${usuario}' type="checkbox" value="">
+                    <label for="${index}" class="form-check-label" for="flexCheckChecked">
+                        ${usuario}
+                    </label>
+                </div>
                 `)
             }
         });
@@ -24,32 +24,61 @@ $(async () => {
 
     await listarUsuarios();
 
-    const registros = async () => {
+    const registros = async (firstTime = false) => {
         let registros = await fetch(`${server}/registro/${fecha.format("YYYYMMDD")}.json`);
         registros = await registros.json();
         registros = registros ? Object.entries(registros) : [];
-        
 
-        let checks = $(".input-registro");
+        $(".input-registro").each(function() {
+            const check = $(this);
 
-        for (const check of checks) {
-            $(check).prop('checked', false);
-            const name  = $(check).data('name');
-            let busqueda = registros.find(([key, value]) => value == name)
-            if(busqueda){
-                $(check).data("key", busqueda[0]);
-                $(check).prop('checked', true);
+            if(firstTime === true){
+                const name = check.data('name');
+                let busqueda = registros.find(([key, value]) => value == name)
+                if(busqueda){
+                    check.data("key", busqueda[0]);
+                    check.prop('checked', true);
+                    $(".listos").append(check.parent());
+                    $(".faltantes").remove(check.parent());
+                }
             }
-        }
+
+            if(check.is(":checked")){
+                // si esta checkeado y esta en faltantes lo saca y lo coloca en listos
+                if(check.parent().parent('.faltantes').length){
+                    $(".listos").append(check.parent());
+                    $(".faltantes").remove(check.parent());
+                    ordenarListas();
+                }
+            }else{
+                // si no esta checkeado y esta en listos lo saca y lo coloca en faltantes
+                if(check.parent().parent('.listos').length){
+                    $(".faltantes").append(check.parent());
+                    $(".listos").remove(check.parent());
+                    ordenarListas();
+                }
+            }
+
+        });
     }
 
-    registros();
+    const ordenarListas = () => {
+        $(".listos").append($(".listos").children().sort(function(a, b) {
+            return $(a).attr("id") > $(b).attr("id")? 1 : -1;
+        }));
+        $(".faltantes").append($(".faltantes").children().sort(function(a, b) {
+            return $(a).attr("id") > $(b).attr("id")? 1 : -1;
+        }));
+    }
+
+    registros(true);
 
     setInterval(() => {
         registros();
     }, 6000);
 
     $(document).on("change",".input-registro", async function() {
+        const check = $(this);
         const valor = $(this).prop('checked');
         const name = $(this).data('name');
         
@@ -64,6 +93,10 @@ $(async () => {
                 });
                 response=await response.json()
                 $(this).data("key", response.name);
+                $(".listos").append(check.parent());
+                $(".faltantes").remove(check.parent());
+                ordenarListas();
+                
             } catch (error) {
                 alert("Ocurrió un error actualizando, intente nuevamente");
                 location.reload();
@@ -72,7 +105,11 @@ $(async () => {
             const key = $(this).data('key');
             if(key){
                 const response = await fetch(`${server}/registro/${fecha.format("YYYYMMDD")}/${key}.json`,{
-                    method: 'DELETE'});
+                    method: 'DELETE'
+                });
+                $(".listos").remove(check.parent());
+                $(".faltantes").append(check.parent());
+                ordenarListas();
             }
         }
     })
